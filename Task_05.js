015363329eb6a9ff38a782124fe49af25d9549de
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2021-12-06 01:34:03 +0400

    Check the validity of password input

---
+++ Task_05.js
@@ -0,0 +1,13 @@
+"use strict;";
+/*Task 05,
+  Write a program to check the validity of password input by users. Validation:
+  
+  1.At least 1 letter between [a-z] and 1 letter between [A-Z].
+  2.At least 1 number between [0-9].
+  3.At least 1 character from [$#@].
+  4.Minimum length 6 characters.
+  5.Maximum length 16 characters.
+
+   Input 12asdf          Output  “Invalid”;
+   Input Aaza1234566#    Output  “Valid”;
+*/
